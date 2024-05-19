dc9087e6ae8b0e39748aca103c236abf1c82bb06
MyNameIsRinax <40387061+MyNameIsRinax@users.noreply.github.com>
2024-05-19 17:22:47 +0800

    change buy notification

--- js/Alert.js
+++ js/Alert.js
@@ -1,8 +1,6 @@
-$(document).ready(function() {
-    $(".alert").hide();
-    $(".buy").click(function showAlert() {
-        $(".alert").fadeTo(2000, 500).slideUp(500, function() {
-        $(".alert").slideUp(500);
-        });
+$(document).ready(function () {
+    $(".buy").click(function showToast() {
+      var myToast = new bootstrap.Toast(document.getElementById("cartToast"));
+      myToast.show();
     });
-});
+  });
